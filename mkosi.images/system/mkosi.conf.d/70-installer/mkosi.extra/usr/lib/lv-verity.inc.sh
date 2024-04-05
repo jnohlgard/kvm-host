b2845d56fc4200e09f4715ba0836dbf98abd22e7
Joakim Nohlgård <joakim@nohlgard.se>
2024-04-05 06:52:13 +0000

    installer: WIP LVM+LUKS OS installation

--- mkosi.images/system/mkosi.conf.d/70-installer/mkosi.extra/usr/lib/lv-verity.inc.sh
+++ mkosi.images/system/mkosi.conf.d/70-installer/mkosi.extra/usr/lib/lv-verity.inc.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/sh
+set -euf
 
+die() {
+  if [ $# -gt 0 ]; then
+    >&2 printf "$@"
+  fi
+  exit 1
+}
+
+# Get the apparent size of a file or block device
+# NB: The actual space consumed by the file may be less than the apparent size
+# because of sparse files and deduplicated extents, so `du` may not give the
+# same result.
 source_size_bytes() {
   [ $# -eq 1 ] || die 'Usage: source_size_bytes <file_or_blockdev>\n'
   f="$1";shift
@@ -12,6 +24,8 @@ source_size_bytes() {
   fi  
 }
 
+# Grab the value part of a kernel command line argument
+# 1 is printed if the argument is provided without value (no "=" component)
 kernel_cmdline_arg() {
   [ $# -eq 1 ] || die 'Usage: kernel_cmdline_arg <search>\n'
 
@@ -23,10 +37,17 @@ kernel_cmdline_arg() {
         printf '%s' "${arg#*=}"
         return
         ;;
+      "${argname}")
+        printf '1'
+        return
+        ;;
     esac
   done
 }
 
+# systemd-repart uses the leftmost 128 bits of the roothash for the partition
+# UUID of the data partition and the rightmost 128 bits of the hash as the
+# partition UUID of the hash partition
 verity_hash_uuid_from_roothash() {
   [ $# -eq 1 ] || die 'Usage: verity_hash_uuid_from_roothash <roothash>\n'
 
@@ -35,7 +56,6 @@ verity_hash_uuid_from_roothash() {
     sed -E \
     -e 's/^.*([0-9a-f]{8})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{12})$/\1-\2-\3-\4-\5/'
 }
-
 verity_data_uuid_from_roothash() {
   [ $# -eq 1 ] || die 'Usage: verity_data_uuid_from_roothash <roothash>\n'
 
@@ -43,4 +63,16 @@ verity_data_uuid_from_roothash() {
   printf '%s' "${roothash}" | \
     sed -E \
     -e 's/^([0-9a-f]{8})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{12}).*$/\1-\2-\3-\4-\5/'
+}
+
+# Get all tags for the given LVs or all LVs
+lv_tags() {
+  /usr/sbin/lvm lvs \
+    --ignorelockingfailure \
+    --readonly \
+    --config='global/wait_for_locks=0' \
+    -o 'tags' \
+    --reportformat=json_std \
+    "$@" \
+    | jq -r '.report[].lv[].lv_tags[]?'
 }
